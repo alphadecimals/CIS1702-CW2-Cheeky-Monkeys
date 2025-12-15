e937deb3e48e95abffb5099709bfcc29e5eb542d
ROSIE FREEMAN <26185318@edgehill.ac.uk>
2025-12-15 11:50:10 +0000

    Testing load and save with dummy json files.

---
+++ main.py
@@ -0,0 +1,36 @@
+import json 
+
+def main():
+    # Testing file management with dummy jsons 
+    items = load_inventory("items_test.json")
+    rooms = load_inventory("rooms_test.json")
+    
+    print(f"Items: {items}")
+    print(f"Room1 original name: {rooms[1]['name']}")
+
+    rooms[1]['name'] = 'PartyRoom!'
+    save_inventory('changes_testing.json', rooms)
+    new_file = load_inventory('changes_testing.json')
+
+    print(f"Altered room name: {new_file[1]['name']}")
+    
+
+def load_inventory(filename):
+    """Safely attempts to open the specified filepath, returning either the JSON file or an empty list. """
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("!! File not found !! Returning empty... ", end="")
+    except json.JSONDecodeError:
+        print("!! File corrupted !! Returning empty... ", end="")
+    return []
+
+def save_inventory(filename, data):
+    print("Attempting to save data... ")
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+        print("Data saved!")
+
+if __name__ == "__main__":
+    main()
