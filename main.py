9bdf6ec737d3caaf2d758805dbb24aa6feeed3db
RosieWitkowski <rosiewitkowski@gmail.com>
2025-12-19 21:34:39 +0000

    Added time taken attribute, player-hp logic and test-case for random hp.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import json 
+import time
+from random import randint
 from input_system import parse_command
+
+"""Note: add docstrings"""
 
 class Player:
     def __init__(self, name):
         self.name = name 
         self.hp = 100
         self.score = 0
+        self.time_score = 0 
     
     def lose_hp(self, damage):
         self.hp -= damage 
-        self.score -= 5
+        self.score -= damage
     
     def gain_hp(self, heal):
         self.hp += heal 
@@ -32,14 +37,26 @@ def game_loop():
 #    game_loop()
 
 def main():
+    time_start = time.time()
+
     items = load_file("items_test.json")
     rooms = load_file("rooms_test.json")
 
     player = Player(input("Username: "))
-    player.lose_hp(20)
-    player.gain_points(200)
-    print(f"{player.name}: {player.score} POINTS")
 
+    # Testing player object and stats logging, points and hp are non-import amounts
+    player.gain_points(200)
+    player.lose_hp(randint(1, 100))
+
+    if player.hp < 1:
+        print("You lost!")
+    else:
+        print("You survived!")
+
+    time_taken = f"{((time.time()) - time_start):.2f} s"
+    player.time_score = time_taken
+
+    print(f"{player.name}: {player.score} POINTS\nTime taken: {time_taken}")
     save_file(f'{player.name}_receipt.json', player.__dict__)
 
 def load_file(filename):
