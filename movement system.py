b67a36e8ea251d7967bed9875d0b55e4ca5bf541
Will-Evans587 <william7777777uu@outlook.com>
2026-01-03 15:15:08 +0000

    Update movement system.py

--- movement system.py
+++ movement system.py
@@ -1,4 +1,9 @@
 coordiantes = [0,0]  #just for testing remove when using in final program
+index = 0 
+
+# gets the index of the room
+while data["Rooms"][index]["gridpos"] != coordiantes:
+    index = index + 1
 
 
 def Movementsystem(coords):
@@ -7,17 +12,31 @@ def Movementsystem(coords):
     '''Moves the player in the specified direction by updating their coordinates based on their inputs.
     Returns the updated coordinates after movement to the main program.'''
     if direction == "north":
-        coords[1] = coords[1] + 1
-        print("you move north")
+        direction = 0
+        possible = Locked_system(index,direction)
+        if possible == True:
+            coords[1] = coords[1] + 1
+            print("you move north")
+
     elif direction[1] == "south":
-        coords = coords[1] - 1
-        print("you move south")
+        direction = 2
+        possible = Locked_system(index,direction)
+        if possible == True:
+            coords = coords[1] - 1
+            print("you move south")
+    
     elif direction[0] == "east":
-        coords = coords[0] - 1
-        print("you move east")
-    elif direction[0] == "west": 
-        coords = coords[0] + 1
-        print("you move west")
+        direction = 1
+        possible = Locked_system(index,direction)
+        if possible == True:
+            coords = coords[0] - 1
+            print("you move east")
+    elif direction[0] == "west":
+        direction = 4
+        possible = Locked_system(index,direction)
+        if possible == True: 
+            coords = coords[0] + 1
+            print("you move west")
     else:
         print("not a valid movement option") 
     return (coords)
