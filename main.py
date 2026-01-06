e848a240614b474456539d0beb3cf26d1a6198d9
RosieWitkowski <rosiewitkowski@gmail.com>
2026-01-06 22:07:55 +0000

    Direction instructions.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def startGame():
             if choice in ["north","east","south","west"]:
              player.coords = movement_system.Movementsystem(player.coords,choice)
             else:
-                print("someone needs to do this bit")
+                print("Please give your direction as 'north', 'east', 'south', 'west'.")
 
     except Exception as e:
         print(f"An error occurred: {e}")
