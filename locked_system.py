9b1394740dae55f5da84dd4a7854fa69d7bc009d
Juana Wesley <jujuwesley05@gmail.com>
2026-01-06 22:47:32 +0000

    Small errors fixed up.

--- locked_system.py
+++ locked_system.py
@@ -8,19 +8,19 @@ def Locked_system(room_index, direction_index, data):
     key_needed = data["Rooms"][room_index]["unlockers"][direction_index]
     #checks if the room is locked
     if status == True:
-        print("the door seems to be locked and needs",key_needed,"to be opened")
+        print("The door seems to be locked and needs ",key_needed," to be opened.")
         # check if you have the key and does the approiate response 
         if key_needed in inventory:
-            print("you had the key and unlocked the door and moved throug the door")
+            print("You had the key and unlocked the door and moved through the door.")
             sucess = True
             data["Rooms"][room_index]["locked"][direction_index] = False  
         elif key_needed == "":
-            print("you can not go this way")        
+            print("You cannot go this way.")        
         else:
-            print("you do not have this key yet and are unable to open this door")
+            print("You do not have this key yet and are unable to open this door.")
     
     else:
-        print("you moved trough the door")
+        print("You moved through the door.")
         sucess = True
         data["Rooms"][room_index]["locked"][direction_index] = False 
     return(sucess) 
